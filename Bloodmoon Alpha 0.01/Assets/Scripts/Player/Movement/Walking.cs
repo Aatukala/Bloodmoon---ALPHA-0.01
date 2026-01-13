b73dfeeaf1764abbd17478c37b12dbfa953955fc
ilmaribc <s2300904@edu.bc.fi>
2026-01-13 12:51:56 +0200

    looking

--- Bloodmoon Alpha 0.01/Assets/Scripts/Player/Movement/Walking.cs
+++ Bloodmoon Alpha 0.01/Assets/Scripts/Player/Movement/Walking.cs
@@ -5,24 +5,21 @@ public class Walking : MonoBehaviour
 {
     public void Move(float speed)
     {
-        Debug.Log("Moving at speed: " + speed);
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("Before Moving Forward: " + transform.position);
-            transform.position += Vector3.forward * speed * Time.deltaTime;
-            Debug.Log("Walking Forward");
+            transform.position += transform.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += -Vector3.forward * speed * Time.deltaTime;
+            transform.position += -transform.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            transform.position += transform.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += -Vector3.right * speed * Time.deltaTime;
+            transform.position += -transform.right * speed * Time.deltaTime;
         }
     }
 }
